86005599e1c2089cf88d4bbfa7917f37abd1a8fb
wwnbb <wwnbb1@gmail.com>
2022-07-10 22:39:27 +0300

    updt

--- cp/cp.go
+++ cp/cp.go
@@ -3,7 +3,8 @@ package cp
 import (
 	"io"
 	"os"
-	// prb "github.com/cheggaaa/pb/v3"
+
+	prb "github.com/cheggaaa/pb/v3"
 )
 
 //go:generate mockgen . os.Create,os.OpenFile
@@ -18,11 +19,11 @@ func CopyFile(from string, to string, offset int, limit int) error {
 		}
 	}
 	defer src.Close()
-	// src_info, err := src.Stat()
+	src_info, err := src.Stat()
 	if err != nil {
 		return err
 	}
-	// fs := src_info.Size() - int64(offset)
+	fs := src_info.Size() - int64(offset)
 
 	if offset > 0 {
 		_, err = src.Seek(int64(offset), 0)
@@ -35,14 +36,14 @@ func CopyFile(from string, to string, offset int, limit int) error {
 		return err
 	}
 
-	// bar := prb.Simple.Start64(fs)
-	// defer bar.Finish()
-	// barReader := bar.NewProxyReader(src)
+	bar := prb.Simple.Start64(fs)
+	defer bar.Finish()
+	barReader := bar.NewProxyReader(src)
 	copy_ := func(limit int) (written int64, err error) {
 		if limit > 0 {
-			return io.CopyN(dst, src, int64(limit))
+			return io.CopyN(dst, barReader, int64(limit))
 		} else {
-			return io.Copy(dst, src)
+			return io.Copy(dst, barReader)
 		}
 	}
 	_, err = copy_(limit)
